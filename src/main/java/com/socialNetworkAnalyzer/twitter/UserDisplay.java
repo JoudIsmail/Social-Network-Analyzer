5a0e6bd1ef4a230c7060ff6d66bb2e0bcd457c1d
JoudIsmail <joud.ismail2@gmail.com>
2020-08-31 22:36:49 +0200

    added new Functions to Instagram analyzer and now working on twitter

--- src/main/java/com/socialNetworkAnalyzer/twitter/UserDisplay.java
+++ src/main/java/com/socialNetworkAnalyzer/twitter/UserDisplay.java
@@ -10,6 +10,7 @@ import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.Cursor;
 
 import java.awt.*;
 import java.io.IOException;
